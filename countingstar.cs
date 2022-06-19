7cb03075a5e6c0898ad550eda58b8a34bcf56714
mincheol Cha <mincheolring93@gmail.com>
2022-06-19 14:15:53 +0900

    Counting_star

---
+++ countingstar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class countingstarstar
+{
+    static void Main()
+    {
+        for(int i =0; i<=4;i++)
+        {
+            for(int j=4; j>=i; j--)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 0; k <= i; k++)
+            {
+                Console.Write("*");
+                if(k>=1)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+
+        }
+       
+    }
+}
